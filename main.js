80d5ee909f3c0e3036624d7f52d0d02047884852
xasiyevsamir <samirakh@code.edu.az>
2023-06-03 13:12:04 +0400

    Js task tamamlandi

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ if (qeydiyyat) {
   } while (!check);
   var passwordcount = 3;
   var balans = 1000;
-  var kredit = 20;
+  var kredit = 0;
   while (passwordcount > 0) {
     var password1 = prompt("Hesaba daxil olmaq ucun Sifrenizi daxil edin !");
     if (password === password1) {
@@ -21,9 +21,9 @@ if (qeydiyyat) {
       do {
         if (kredit != 0 && confirm("Kredit odeneisi edeceksiz ?")) {
           do {
-            console.log(`Balansiniz:${balans}`);
+            console.log(`Balansiniz:${balans} Tarix:${new Date()}`);
             do {
-              var data = Number(prompt("Mebleg."));
+              var data = Number(prompt(`Sizin balansiniz:${balans}`));
               check = Boolean(data);
               if (!check || data <= 0) {
                 alert("Yalniz musbet reqem daxil edin !");
@@ -31,29 +31,30 @@ if (qeydiyyat) {
             } while (data < 0 || !check);
             if (balans < data) {
               console.error("Balansiniz yeterli deyil.");
-              alert(`Odenis etmek ucun ${balans}-AZN daxil edin.`);
+              alert(`Odenis etmek ucun max ${balans}-AZN daxil edin.`);
               continue;
             } else {
-              console.log(data);
-              console.log(balans);
-              console.log(kredit);
-
               if (data > kredit) {
+                balans -= data;
+                kredit -= data;
                 balans = balans - kredit;
                 kredit = 0;
+                console.log(`Kredit odenisi bitdi. Tarix:${new Date()}`);
+              } else {
+                balans -= data;
+                kredit -= data;
               }
-              balans -= data;
-              kredit -= data;
               console.log("Odenis ugurludur !");
               console.log(`Qalan kredit borcu:${kredit}`);
               if (kredit != 0) {
                 var data1 = confirm("Yeniden odenis etmek isdiyirsiz ?");
               } else {
-                console.log("Kredit odenisi bitdi.");
                 if (confirm("Yeniden kredit goturmek isdiyirsiz ?")) {
                   kredit = maais * 0.45 * 12;
                   balans += kredit - (kredit * 2) / 100;
-                  console.log(`Qalan kredit borcu:${kredit}`);
+                  console.log(
+                    `Qalan kredit borcu:${kredit} Tarix:${new Date()}`
+                  );
                   break;
                 } else {
                   break;
@@ -61,11 +62,11 @@ if (qeydiyyat) {
               }
             }
           } while (data1);
-        } else if (balans != 0 && confirm("Pul cixarmaq ?")) {
+        } else if (confirm("Pul cixarmaq ?")) {
           do {
-            console.log(`Balansiniz:${balans}`);
+            console.log(`Balansiniz:${balans} Tarix:${new Date()}`);
             do {
-              var data = Number(prompt("Mebleg."));
+              var data = Number(prompt(`Sizin balansiniz:${balans}`));
               check = Boolean(data);
               if (!check || data <= 0) {
                 alert("Yalniz musbet reqem daxil edin !");
@@ -73,7 +74,9 @@ if (qeydiyyat) {
             } while (data < 0 || !check);
             if (data <= balans) {
               balans -= data;
-              console.log(`${data}-AZN odenildi  Balans:${balans}`);
+              console.log(
+                `${data}-AZN odenildi  Balans:${balans} Tarix:${new Date()}`
+              );
               if (confirm("Odenisi tekirarla.")) {
                 if (balans != 0) {
                   continue;
@@ -82,32 +85,42 @@ if (qeydiyyat) {
                   if (kredit === 0 && confirm("Kredit goturmek isdiyirsiz ?")) {
                     kredit = maais * 0.45 * 12;
                     balans += kredit - (kredit * 2) / 100;
-                    console.log(`Qalan kredit borcu:${kredit}`);
+                    console.log(
+                      `Qalan kredit borcu:${kredit} Tarix:${new Date()}`
+                    );
                     if (balans != 0 && confirm("Yeniden odenis et.")) {
                       continue;
                     } else {
                       break;
                     }
-                  } else {
+                  } else if (kredit != 0) {
                     console.log("Sizin artiq kreditiniz var.");
+                    break;
+                  } else {
+                    console.alert("Sizin artiq kreditiniz var.");
                     break;
                   }
                 }
               } else {
+                console.alert("Odenisi tekirarlamaqdan imtina etdiz.");
                 break;
               }
             } else {
               if (kredit === 0 && confirm("Kredit goturmek isdiyirsiz ?")) {
                 kredit = maais * 0.45 * 12;
                 balans += kredit - (kredit * 2) / 100;
-                console.log(`Qalan kredit borcu:${kredit}`);
-                if (balans != 0 && confirm("Yeniden odenis et.")) {
+                console.log(`Qalan kredit borcu:${kredit} Tarix:${new Date()}`);
+                if (confirm("Yeniden odenis et.")) {
                   continue;
                 } else {
+                  console.alert("Odenisi tekirarlamaqdan imtina etdiz.");
                   break;
                 }
-              } else {
+              } else if (kredit != 0) {
                 console.log("Sizin artiq kreditiniz var.");
+                break;
+              } else {
+                console.alert("Siz kredit goturmek isdemediz.");
                 break;
               }
             }
@@ -116,17 +129,20 @@ if (qeydiyyat) {
           alert("Hesabdan cixis olundu !");
           break;
         }
-      } while (kredit != 0);
+      } while (true);
     } else {
       if (!confirm("Sifre yanlış,Yeniden cehd edin !")) {
-        alert("Hesabdan cixis olundu !");
+        alert("Hesabdan cixis olundu seciz !");
         break;
-      }
-      passwordcount--;
-      console.log(`Cehd sayi:${passwordcount}`);
-      if (passwordcount === 0) {
-        alert("Kart bloklandi,Banka muraciet et");
-        break;
+      } else {
+        passwordcount--;
+        console.log(`Cehd sayi:${passwordcount}`);
+        if (passwordcount === 0) {
+          alert("Kart bloklandi,Banka muraciet et");
+          break;
+        } else {
+          continue;
+        }
       }
     }
   }
@@ -134,6 +150,7 @@ if (qeydiyyat) {
   alert("Qeydiyat dayandirildi !");
 }
 
+//Ulduz taskininin helli.
 /*var data;
 for (var i = 0; i < 9; i++) {
   data = "";
